4ce9a8915c9b52817bb2ae96519e563241bc0934
anvarbek001 <ergashevanvarbek00700@gmail.com>
2025-04-04 21:35:14 +0500

    reactcrud

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import Context from './components/Context'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Context /> 
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/appData.js
@@ -0,0 +1,29 @@
+export const rowData = [
+    {
+        id: 1,
+        title: 'Car',
+        price:10,
+        company: 'Honda',
+        info: 'One of the best Car',
+        inCart:false,
+        count:1,
+    },
+    {
+        id: 2,
+        title: 'Bike',
+        price:20,
+        company: 'Totyota',
+        info: 'One of the best Bike',
+        inCart:false,
+        count:1,
+    },
+    {
+        id: 3,
+        title: 'Bicycle',
+        price:30,
+        company: 'Honda',
+        info: 'One of the best Bicycle',
+        inCart:false,
+        count:1,
+    }
+]
